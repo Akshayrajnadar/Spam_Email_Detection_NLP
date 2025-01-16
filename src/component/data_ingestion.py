e183cead5ac6fd763b9e404602a1be16f5e5bb32
Akshayraj Nadar <akshayraj08032000@gmail.com>
2025-01-16 16:22:43 +0530

    Prepared the model of data ingestion and data transformation

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -12,3 +12,24 @@ from src.component.data_transformation import DataTransformationConfig
 
 from src.component.model_training import ModelTrainerConfig
 from src.component.model_training import ModelTrainer
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str= os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__=="__main__":
+    pass
